b5a35e141fc454e5868c74ce9ce0932b42da5948
Danil Zhelnov <danil.zhelnov@graph.uk>
2019-02-06 13:28:42 +0500

    Detect OS version

--- vars/nugetAudit.groovy
+++ vars/nugetAudit.groovy
@@ -5,6 +5,10 @@ def call(body = null) {
 	def auditHome = tool name: 'DevAudit', type: 'com.cloudbees.jenkins.plugins.customtools.CustomTool'
 
 	for(file in files) {
-		bat label: "Nuget Audit: ${file}", script: "${auditHome}/DevAudit/devaudit nuget --file ${file} --ci --non-interactive"
+		if (isUnix()) {
+			sh label: "Nuget Audit: ${file}", script: "mono ${auditHome}/DevAudit/devaudit.exe nuget --file ${file} --ci --non-interactive"
+		} else {
+			bat label: "Nuget Audit: ${file}", script: "${auditHome}/DevAudit/devaudit nuget --file ${file} --ci --non-interactive"
+		}
 	}
 }
