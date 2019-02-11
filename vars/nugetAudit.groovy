0cf18ea3562ebe31e822b3f9d43271ca471e3cc7
Danil Zhelnov <danil.zhelnov@graph.uk>
2019-02-11 22:56:20 +0500

    Split nuget audit and netcore audit

--- vars/nugetAudit.groovy
+++ vars/nugetAudit.groovy
@@ -1,14 +1,22 @@
-def call(body = null) {
-	def projFiles = findFiles(glob: '**/*.csproj')
-	def packageFiles = findFiles(glob: '**/packages.config')
-	def files = projFiles + packageFiles
-	def auditHome = tool name: 'DevAudit', type: 'com.cloudbees.jenkins.plugins.customtools.CustomTool'
+def call(String auditTarget = 'nuget') {
+	def patterns = [
+		nuget:'**/*.csproj',
+		netcore: '**/packages.config'
+	
+	
+	def pattern = patterns[auditTarget]
 
-	for(file in files) {
-		if (isUnix()) {
-			sh label: "Nuget Audit: ${file}", script: "mono ${auditHome}/DevAudit/devaudit.exe nuget --file ${file} --ci --non-interactive"
-		} else {
-			bat label: "Nuget Audit: ${file}", script: "${auditHome}/DevAudit/devaudit nuget --file ${file} --ci --non-interactive"
+	if (pattern != null) {
+		def files = findFiles(glob: pattern)
+
+		def auditHome = tool name: 'DevAudit', type: 'com.cloudbees.jenkins.plugins.customtools.CustomTool'
+
+		for(file in files) {
+			if (isUnix()) {
+				sh label: "Nuget Audit: ${file}", script: "mono ${auditHome}/DevAudit/devaudit.exe ${auditTarget} --file ${file} --ci --non-interactive"
+			} else {
+				bat label: "Nuget Audit: ${file}", script: "${auditHome}/DevAudit/devaudit ${auditTarget} --file ${file} --ci --non-interactive"
+			}
 		}
 	}
 }
